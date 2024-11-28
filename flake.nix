434eb7bddb355a5ac3ffaa7434c959352a9d45b9
prehonor <ryx19891207@163.com>
2024-11-28 10:05:48 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,11 +12,11 @@
       downloadUrl = {
         "specific" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-	  sha256 = "";
+	  sha256 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 	};
 	"generic" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-	  sha256 = "";
+	  sha256 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 	};
       };
 
